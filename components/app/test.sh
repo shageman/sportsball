89e18e350162823cb8ea9ef7ae9697fa9b5cbd7e
Stephan Hagemann <stephan.hagemann@gmail.com>
2015-02-26 08:26:46 -0500

    Fix spec runner

--- components/app/test.sh
+++ components/app/test.sh
@@ -6,7 +6,7 @@ echo "*** Running teaser engine specs"
 bundle install | grep Installing
 bundle exec rake db:create db:migrate
 RAILS_ENV=test bundle exec rake db:create db:migrate
-bundle exec rspec spec/controllers
+bundle exec rspec spec
 exit_code+=$?
 
 exit $exit_code
