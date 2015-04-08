c10fefb235dd19a73243976390163794cc3bf92a
Stephan Hagemann <stephan.hagemann@gmail.com>
2015-04-08 14:42:51 -0600

    Work around quirk of db:migrate not working directly after db:create

--- components/app/test.sh
+++ components/app/test.sh
@@ -5,7 +5,8 @@ exit_code=0
 echo "*** Running app engine specs"
 bundle install | grep Installing
 bundle exec rake db:create db:migrate
-RAILS_ENV=test bundle exec rake db:create db:migrate
+RAILS_ENV=test bundle exec rake db:create
+RAILS_ENV=test bundle exec rake db:migrate
 bundle exec rspec spec
 exit_code+=$?
 
--- test.sh
+++ test.sh
@@ -5,8 +5,10 @@ exit_code=0
 echo "*** Running container app specs"
 bundle install | grep Installing
 bundle exec rake db:drop
-bundle exec rake db:create db:migrate
-RAILS_ENV=test bundle exec rake db:create db:migrate
+bundle exec rake db:create
+bundle exec rake db:migrate
+RAILS_ENV=test bundle exec rake db:create
+RAILS_ENV=test bundle exec rake db:migrate
 bundle exec rspec spec
 exit_code+=$?
 
